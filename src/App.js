2f69c389e02c5868e1447de71a50a0f52c3d957e
zoexiong <xiongsy@uw.edu>
2017-09-29 00:35:25 -0700

    add sortable feature for project detail view

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// import ProjectList from './components/ProjectList/ProjectList';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <div className="title">
-//             <img src={logo} className="App-logo" alt="logo" />
-//             <h2>Task Management</h2>
-//           </div>
-//         </div>
-//         <div className="project-list">
-//           <ProjectList/>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import uniqueId from 'lodash/uniqueId';
-import React from 'react';
-import Sortable from 'react-sortablejs';
+import ProjectList from './components/ProjectList/ProjectList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="title">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Task Management</h2>
+          </div>
+        </div>
+        <div className="project-list">
+          <ProjectList/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// import uniqueId from 'lodash/uniqueId';
+// import React from 'react';
+// import Sortable from 'react-sortablejs';
 
 // Functional Component
-const SharedGroup = ({ items }) => {
-    items = items.map(val => (<li key={uniqueId()} data-id={val}>{val}</li>));
-
-    return (
-        <Sortable
-            // See all Sortable options at https://github.com/RubaXa/Sortable#options
-            options={{
-                group: 'shared'
-            }}
-            tag="ul"
-        >
-            {items}
-        </Sortable>
-    );
-};
-
-export default SharedGroup;
+// const SharedGroup = ({ items }) => {
+//     items = items.map(val => (<li key={uniqueId()} data-id={val}>{val}</li>));
+//
+//     return (
+//         <Sortable
+//             // See all Sortable options at https://github.com/RubaXa/Sortable#options
+//             options={{
+//                 group: 'shared'
+//             }}
+//             tag="ul"
+//         >
+//             {items}
+//         </Sortable>
+//     );
+// };
+//
+// export default SharedGroup;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SharedGroup from './App';
-
-const App = (props) => {
-    return (
-        <div>
-            <SharedGroup
-                items={['Apple', 'Banaba', 'Cherry', 'Grape']}
-            />
-            <br/>
-            <SharedGroup
-                items={['Lemon', 'Orange', 'Pear', 'Peach']}
-            />
-        </div>
-    );
-};
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import SharedGroup from './App';
+
+// const App = (props) => {
+//     return (
+//         <div>
+//             <SharedGroup
+//                 items={['Apple', 'Banaba', 'Cherry', 'Grape']}
+//             />
+//             <br/>
+//             <SharedGroup
+//                 items={['Lemon', 'Orange', 'Pear', 'Peach']}
+//             />
+//         </div>
+//     );
+// };
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
